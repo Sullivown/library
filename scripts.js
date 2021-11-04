59785c7b43b3a4d85f36c620ee104afd051304af
Sullivown <daniel.w.t.sullivan@gmail.com>
2021-11-04 14:16:50 +0000

    add genres and colors

--- scripts.js
+++ scripts.js
@@ -3,6 +3,28 @@ function Library() {
     // Tracks books currently in the library
     this.books = [];
 
+    this.settings = {
+        currentSort: 'default',
+        genres: {
+            other: {
+                name: 'Other',
+                color: '#000000'
+            },
+            thriller: {
+                name: 'Thriller',
+                color: '#FFC300',
+            },
+            horror: {
+                name: 'Horror',
+                color: '#581845',
+            },
+            historical: {
+                name: 'Historical',
+                color: '#AF9793',
+            },
+        }
+    }
+
     // Refreshes the shelf display
     this.refreshDisplay = function() {
         shelf.innerHTML = '';
@@ -13,8 +35,8 @@ function Library() {
     };
 
     // Adds book to the library and adds it to the shelf
-    this.addBook = function(title, author, pages, read) {
-        const newBook = new Book(title, author, pages, read);
+    this.addBook = function(title, author, genre, pages, read, rating) {
+        const newBook = new Book(title, author, genre, pages, read, rating);
         myLibrary.books.push(newBook);
         createBookCard(newBook);
     };
@@ -41,11 +63,13 @@ function Library() {
     };
 }
 
-function Book(title, author, pages, read) {
+function Book(title, author, genre, pages, read, rating) {
     this.title = title;
     this.author = author;
+    this.genre = genre;
     this.pages = pages;
     this.read = read;
+    this.rating = rating;
 }
 
 // Create the library
@@ -59,13 +83,23 @@ const header = document.querySelector('header');
 
 // Add book button handler
 addNewButton.addEventListener('click', () => {
+    let genreOptionsArr = [];
+    for (const property in myLibrary.settings.genres) {
+        genreOptionsArr.push(`<option value="${property}">${myLibrary.settings.genres[property].name}</option>`);
+    };
+
     if (addNewButton.textContent === 'Add New Book') {
         let addBookBanner = document.createElement('div');
         addBookBanner.classList.add('addBookBanner');
         addBookBanner.innerHTML = `
             <input type="text" id="title" name="title" placeholder="Title">
             <input type="text" id="author" name="author" placeholder="Author">
+            <select id="genre" name="genre" placeholder="Total Pages">
+                <option value="">--Please choose an genre--</option>
+                ${genreOptionsArr}
+            </select>
             <input type="number" id="pages" name="pages" placeholder="Total Pages">
+            <input type="number" id="rating" name="rating" placeholder="My Rating">
             <div>
                 <label for="read">Read?</label><input type="checkbox" id="read" name="read">
             </div>
@@ -78,9 +112,10 @@ addNewButton.addEventListener('click', () => {
         document.querySelector('#submit-new-book').addEventListener('click', () => {
             const title = document.querySelector('#title');
             const author = document.querySelector('#author');
+            const genre = document.querySelector('#genre');
             const pages = document.querySelector('#pages');
             const read = document.querySelector('#read');
-            myLibrary.addBook(title.value, author.value, pages.value, read.checked);
+            myLibrary.addBook(title.value, author.value, genre.value, pages.value, read.checked);
 
             // Reset input values
             title.value = '';
@@ -104,11 +139,16 @@ function createBookCard(book) {
             bookDiv.innerHTML = `
             <h3>${book.title}</h3>
             <div>by ${book.author}</div>
+            <div>Genre: ${myLibrary.settings.genres[book.genre].name}</div>
             <div>Pages: ${book.pages}</div>
+            <div>My Rating: ${book.rating}</div>
             <div class="card-buttons-div">
                 <button class="read-button ${book.read ? 'read' : 'unread'}">${book.read ? 'Read' : 'Unread'}</button>
                 <button class="delete-button">Delete</button>
             </div>`;
+
+            // Add genre colours
+            bookDiv.style.boxShadow = `inset 0 5px ${myLibrary.settings.genres[book.genre].color}`
 
             shelf.append(bookDiv);
 
@@ -127,8 +167,7 @@ function createBookCard(book) {
 // Function to populate the library with data for testing
 function populateTestData(numOfBooks) {
     for (let i = 1; i <= numOfBooks; i++) {
-        const testBook = new Book(`Title ${i}`, `Author ${i}`, i * 100, i % 3 === 0 ? false : true);
-        myLibrary.books.push(testBook);
+       myLibrary.addBook(`Title ${i}`, `Author ${i}`, `thriller`, i * 100, i % 3 === 0 ? false : true, 5);
     }
 }
 
